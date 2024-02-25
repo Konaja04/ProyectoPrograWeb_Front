195c4c98d407cbc8e04a6b0b69ab0fd08fbeed77
Konaha <Konaha@email.com>
2024-02-25 15:51:22 -0500

    cartelera conexion con back

--- src/Views/Inicio/components/Cartelera.jsx
+++ src/Views/Inicio/components/Cartelera.jsx
@@ -7,39 +7,21 @@ import Carousel from 'react-material-ui-carousel';
 import { Link } from 'react-router-dom';
 import ConfirmationNumberOutlinedIcon from '@mui/icons-material/ConfirmationNumberOutlined';
 
-const obtenerPeliculasRandom = (lista, cantidad) => {
-    const peliculasClon = lista.slice();
-    cantidad = Math.min(cantidad, peliculasClon.length);
-    const peliculasRandom = [];
 
-    for (let i = 0; i < cantidad; i++) {
-        const indiceRandom = Math.floor(Math.random() * peliculasClon.length);
-        const peliculaSeleccionada = peliculasClon.splice(indiceRandom, 1)[0];
-        peliculasRandom.push(peliculaSeleccionada);
-    }
-
-    return peliculasRandom;
-}
 
 const Cartelera = () => {
     const { path } = useParams();
-    const [sala, setDataSala] = useState({})
     const [pelis, setDataPelis] = useState([])
 
-    const obtenerSala = async () => {
+    const obtenerCartelera = async () => {
 
-        const response = await fetch("https://raw.githubusercontent.com/ulima-pw/data-20240/main/salasv2.json")
-        const responsePelis = await fetch("https://raw.githubusercontent.com/ulima-pw/data-20240/main/peliculas_limpio.json")
-        const data = await response.json()
-        const dataPelis = await responsePelis.json()
-        setDataSala(data.filter((sala) => {
-            return sala.path == path
-        })[0])
-        setDataPelis(obtenerPeliculasRandom(dataPelis, 12))
+        const response = await fetch("http://127.0.0.1:8000/salas_cine/cartelera")
+        const dataPelis = await response.json()
+        setDataPelis(dataPelis)
 
     }
     useEffect(() => {
-        obtenerSala()
+        obtenerCartelera()
     }, [])
 
     return <Container sx={{ py: 12 }} style={{ marginTop: '160px' }} maxWidth="md">
